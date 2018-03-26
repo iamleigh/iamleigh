1abf8df576d6eb19fbfd08bca396234d2a14dba3
Leighton <leighsapir@icloud.com>
2018-03-26 16:43:26 -0500

    [Update] Filter the page title

--- functions.php
+++ functions.php
@@ -34,7 +34,21 @@ function iamleigh_setup() {
 
 add_action( 'after_setup_theme', 'iamleigh_setup' );
 
-// Filter the page title
+/**
+ *
+ * Filter the page title.
+ *
+ * Creates a nicely formatted and more specific title element text
+ * for output in head of document, based on current view.
+ *
+ * @since iamleigh 1.0
+ * @version 1.0
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string Filtered title.
+ *
+ */
 function iamleigh_wp_title( $title, $sep ) {
     
     global $paged, $page;
