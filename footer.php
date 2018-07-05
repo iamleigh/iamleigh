f121cdf6ca9495a5881d5db556c8d71cabac1487
Leighton <shiasapir@gmail.com>
2018-07-05 09:22:25 -0500

    [fix] phpcs

--- footer.php
+++ footer.php
@@ -20,17 +20,17 @@
 
 					<div class="iamui-container">
 
-						<p class="iamui-cta--text"><?php _e( 'Let\'s talk about your project', 'iamleigh' ); ?></p>
+						<p class="iamui-cta--text"><?php esc_html_e( "Let's talk about your project", 'iamleigh' ); ?></p>
 
-						<?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
-						
-						<?php if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) { ?>
-							
-							<button class="iamui-button iamui-button--cyan iamui-cta--button iamui-modal--open" data-modal="footer-popup-contact"><?php _e( 'Let\'s Engage', 'iamleigh' ); ?></button>
+						<?php include_once ABSPATH . 'wp-admin/includes/plugin.php'; ?>
+
+						<?php if ( is_plugin_active( 'forminator/forminator.php' ) ) { ?>
+
+							<button class="iamui-button iamui-button--cyan iamui-cta--button iamui-modal--open" data-modal="footer-popup-contact"><?php esc_html_e( "Let's Engage", 'iamleigh' ); ?></button>
 
 						<?php } else { ?>
-							
-							<a href="mailto:leighton@pandamints.com" class="iamui-button iamui-button--cyan iamui-cta--button"><?php _e( 'Let\'s Engage', 'iamleigh' ); ?></a>
+
+							<a href="mailto:leighton@pandamints.com" class="iamui-button iamui-button--cyan iamui-cta--button"><?php esc_html_e( "Let's Engage", 'iamleigh' ); ?></a>
 
 						<?php } ?>
 
@@ -45,9 +45,9 @@
 						<div class="iamui-row">
 
 							<div class="iamui-col col-12 col-md-6">
-								
+
 								<p><?php printf( __( '%s © Leighton Sapir', 'iamleigh' ), date('Y') ); ?></p>
-								
+
 							</div>
 
 							<div class="iamui-col col-12 col-md-6">
@@ -55,14 +55,16 @@
 								<?php if ( has_nav_menu( 'footer' ) ) : ?>
 
 									<nav class="iamui-navigation--footer" role="navigation">
-										
-										<?php wp_nav_menu( array(
+
+										<?php
+										wp_nav_menu( array(
 											'theme_location' => 'footer',
 											'menu_id'        => 'iamui-nav--footer',
-										) ); ?>
+										) );
+										?>
 
 									</nav>
-									
+
 								<?php endif; ?>
 
 							</div>
@@ -78,8 +80,8 @@
 		</div>
 
 		<?php if ( is_plugin_active( 'contact-form-7/wp-contact-form-7.php' ) ) { ?>
-			
-			<?php get_template_part( "template-parts/modals/modal", "contact" ); ?>
+
+			<?php get_template_part( 'template-parts/modals/modal', 'contact' ); ?>
 
 		<?php } ?>
 
